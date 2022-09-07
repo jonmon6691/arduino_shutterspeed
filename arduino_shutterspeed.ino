4d77392aa22fc29c7b59e258e721ce386f5b978f
jonmon6691 <jonmon6691@gmail.com>
2022-09-06 18:35:00 -0700

    Code cleanup

--- arduino_shutterspeed.ino
+++ arduino_shutterspeed.ino
@@ -2,7 +2,7 @@
 // Jon Wallace 2022
 
 #define LIGHT_SENSOR (A0)
-#define CAL_BUTTON_SHUTTER_CLOSED (19)
+#define CAL_BUTTON_SHUTTER_CLOSED (19) // Marked A5
 #define CAL_BUTTON_SHUTTER_OPEN (10)
 
 #define STATE_CLOSED 1
@@ -11,7 +11,7 @@
 volatile int shutter_closed_cal = 1024;
 volatile int shutter_open_cal = 0;
 volatile int trigger = 512;
-volatile int last_state = STATE_CLOSED;
+volatile int state = STATE_CLOSED;
 volatile unsigned long open_time = 0;
 
 void setup() {
@@ -25,29 +25,34 @@ void loop() {
 
     if (digitalRead(CAL_BUTTON_SHUTTER_CLOSED) == 0) {
       shutter_closed_cal = sensorValue;
-      set_triggers();
-    } else if (digitalRead(CAL_BUTTON_SHUTTER_OPEN) == 0) {
+      update_trigger();
+    }
+    
+    if (digitalRead(CAL_BUTTON_SHUTTER_OPEN) == 0) {
       shutter_open_cal = sensorValue;
-      set_triggers();
+      update_trigger();
     }
 
-    if (sensorValue > trigger && last_state == STATE_OPEN) {
+    if (sensorValue > trigger && state == STATE_OPEN) {
       Serial.println(micros() - open_time);
-      last_state = STATE_CLOSED;
-    } else if (sensorValue <= trigger && last_state == STATE_CLOSED) {
-      last_state = STATE_OPEN;
+      state = STATE_CLOSED;
+    }
+    
+    if (sensorValue <= trigger && state == STATE_CLOSED) {
+      state = STATE_OPEN;
       open_time = micros();
     }
 
 }
 
-void set_triggers()
+void update_trigger()
 {
+  // Bias the trigger point towards the open condition since the sensor has a long tail to reset to the closed value
   trigger = (shutter_closed_cal + 5*shutter_open_cal) / 6;
+  
   Serial.print(shutter_closed_cal);
   Serial.print(",");
   Serial.print(trigger);
   Serial.print(",");
   Serial.println(shutter_open_cal);
-  return;
 }
